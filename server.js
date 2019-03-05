c0c9aab8a01f9676d7f40b40f19cc2868f38f160
Heta <hpsitp@gmail.com>
2019-03-05 12:23:40 -0800

    added express.js

--- server.js
+++ server.js
@@ -1,47 +1,26 @@
-var http = require('http');
-var fs = require('fs');
 var path = require('path');
-// function onRequest(request, response){
-//     response.writeHead(200, {'Content-Type' : 'text/html'});
-//     // response.write('Hello Bitbroker');
-//     // response.end();
-//     var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-//     myReadStream.pipe(response);
-// }
-// http.createServer(onRequest).listen(8000,'127.0.0.1');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-http.createServer(function(req,res){
-    if(req.url === "/"){
-        fs.readFile("./public/index.html","UTF-8", function(err, html){
-            res.writeHead(200, {"Content-Type" : "text/html"});
-            res.end(html);
-        });
-    }else if(req.url.match("\.css$")){
-        var cssPath = path.join(__dirname,'public',req.url);
-        // console.log(csspath);
-        var fileStream = fs.createReadStream(cssPath,"UTF-8");
-        res.writeHead(200,{"Content-Type": "text/css"} );
-        fileStream.pipe(res);
-    }else if(req.url.match("\.jpg$")){
-        var imagePath1 = path.join(__dirname,'public',req.url);
-        var fileStream = fs.createReadStream(imagePath1);
-        res.writeHead(200,{"Content-Type": "image/jpg"} );
-        fileStream.pipe(res);
-    }else if(req.url.match("\.js$")){
-        var jsPath = path.join(__dirname,'public',req.url);
-        var fileStream = fs.createReadStream(jsPath);
-        res.writeHead(200,{"Content-Type": "application/javascript"} );
-        fileStream.pipe(res);
-    }else if(req.url.match("\.png$")){
-        var imagePath2 = path.join(__dirname,'public',req.url);
-        var fileStream = fs.createReadStream(imagePath2);
-        res.writeHead(200,{"Content-Type": "image/png"} );
-        fileStream.pipe(res);
-    }
-    else{
-        res.writeHead(404, {"Content-Type" : "text/html"});
-        res.end("No page found"); 
-    }
+
+var logger = function(req, res, next){
+    console.log('logging..');
+    next();
+}
+
+app.use(logger);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set static path
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/',function(req,res){
+    res.send('Hello World');
+})
+app.listen(8080,function(req,res){
+    console.log('listening to port 8080...');
+});
+    
    
-    // console.log(req.url);
-}).listen(8080);
+
